0d2f5799673c53c7f12867d5c543f684a010d68d
Lukasz <lukasz.szupka@student.fh-kiel.de>
2024-09-22 16:59:30 +0200

    the falling_objects get now a random image

--- main_scene.gd
+++ main_scene.gd
@@ -3,6 +3,10 @@ extends Node2D
 # Preload the FallingObject scene
 @export var falling_object_scene: PackedScene = preload("res://falling_objects/falling_object.tscn")
 
+# Preload the textures for random selection
+var bad_comment_1: Texture2D = preload("res://image/comments/bad/BadComment1.png")
+var bad_comment_2: Texture2D = preload("res://image/comments/bad/BadComment2.png")
+
 # Variables for spawning objects
 var spawn_timer: float = 2.0  # Time in seconds between each spawn
 var spawn_interval: float = 2.0  # Reset value for the timer
@@ -28,6 +32,13 @@ func _process(delta):
 func spawn_object():
 	# Instantiate the falling object scene
 	var falling_object = falling_object_scene.instantiate()
+
+	# Randomly select one of the two textures
+	var random_texture = bad_comment_1 if randf() > 0.5 else bad_comment_2
+
+	# Set the texture
+	var sprite = falling_object.get_node("Sprite2D")  
+	sprite.texture = random_texture
 	
 	# Random X position for the object to spawn within the screen width
 	var random_x_position = randi() % int(screen_size.x)
