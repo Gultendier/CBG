70d8689640f6c640c7c6e57d4cace86b4347dafb
Lukasz <Lukasz.szupka@student.fh-kiel.de>
2024-10-04 22:27:30 +0200

    changed project structure and added a transition

--- main_menu.gd
+++ main_menu.gd
@@ -12,7 +12,9 @@ func _process(delta: float) -> void:
 
 
 func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://main_scene.tscn")
+	TransitionScreen.transition()
+	await TransitionScreen.on_transition_finished
+	get_tree().change_scene_to_file("res://main_scene/main_scene.tscn")
 
 func _on_options_pressed() -> void:
 	pass # Replace with function body.
