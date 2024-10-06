eee8ee0af29fe658393328e6992fae829930edd1
Lukasz <Lukasz.szupka@student.fh-kiel.de>
2024-10-06 21:37:53 +0200

    new icon and structure changes

--- main_scene/main_scene.gd
+++ main_scene/main_scene.gd
@@ -6,8 +6,8 @@ extends Node2D
 # Variables for spawning objects
 var spawn_timer: float = 1.0  # Time in seconds between each spawn
 var spawn_interval: float = 2.0  # Reset value for the timer
-var screen_size: Vector2 # Screen bounds to control where the objects are spawned
 var spawn_y: float = -50
+var screen_size: Vector2 # Screen bounds to control where the objects are spawned
 
 func _ready():
 	screen_size = get_viewport_rect().size 	# Get the screen size
@@ -25,24 +25,17 @@ func _process(delta):
 func spawn_object():
 	# Instantiate the falling object scene
 	var falling_object = falling_object_scene.instantiate()
-	# Get the viewport rectangle
 	var viewport = get_viewport_rect()
 	# Calculate the center point
 	var center_x = viewport.size.x * 0.5
 	# Determine the distance from the center (1/4 of the screen width)
 	var distance_from_center = viewport.size.x * 0.25
-	 # Generate random offsets
 	var random_offset_x = randf_range(-distance_from_center, distance_from_center)
 	# Calculate the final spawn position
 	var spawn_position = Vector2(center_x + random_offset_x, spawn_y)
-	# Set the position of the falling object
-	falling_object.position = spawn_position
-	# Start falling animation
-	
+	falling_object.position = spawn_position	
 	# Add the object as a child of the main scene
 	add_child(falling_object)
-
-
 
 func update_score_label():
 	$CanvasLayer/HBoxContainer/ScoreBoard.text = "Score: " + str(ScoreBoard.score) # Update the Label's text to reflect the global score
