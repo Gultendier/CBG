aab2fc36df0cf9eeee5b614adb695bf38186d9c6
Lukasz <Lukasz.szupka@student.fh-kiel.de>
2024-10-06 13:17:03 +0200

    fixed the animation and added the game task to it

--- transition_screen.gd
+++ transition_screen.gd
@@ -4,9 +4,11 @@ signal on_transition_finished
 
 @onready var color_rect = $ColorRect
 @onready var animation_player = $AnimationPlayer
+@onready var label = $Label
 
 func _ready():
 	color_rect.visible = false
+	label.visible = false
 	animation_player.animation_finished.connect(_on_animation_finished)
 	
 func _on_animation_finished(animation_name):
@@ -15,6 +17,8 @@ func _on_animation_finished(animation_name):
 		animation_player.play("fade_to_normal")
 	elif animation_name == "fade_to_normal":
 		color_rect.visible = false
+		label.visible = false
 func transition():
 	color_rect.visible = true
+	label.visible = true
 	animation_player.play("fade_to_black")
