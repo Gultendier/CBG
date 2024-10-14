cf640bc5c7eb3d02e7764c03342522bf2801ab47
Gultendier <108030664+Gultendier@users.noreply.github.com>
2024-10-14 22:50:34 +0200

    Add files via upload
    during clean up this file was deleted

---
+++ game_progress.gd
@@ -0,0 +1,26 @@
+extends Node
+
+# variables for falling speed
+var falling_speed: float = 100
+
+# variables for the game progress
+var grabbed_for_the_first_time = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+	
+func increase_speed():
+	falling_speed += 5
+	
+func trigger_dialog_one():
+	GameProgress.grabbed_for_the_first_time = true
+	get_tree().paused = true
+	print("paused")
+	var main_scene = get_tree().current_scene
+	if main_scene.has_method("show_dialog_one"):
+		main_scene.show_dialog_one()
