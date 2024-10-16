dfc2b45e498b5cc40797674f5b3fde9db77e9a80
Lukasz <Lukasz.szupka@student.fh-kiel.de>
2024-10-16 17:53:19 +0200

    dialog one trigger now after 3 connections

--- falling_objects/falling_object.gd
+++ falling_objects/falling_object.gd
@@ -91,6 +91,7 @@ func _on_area_entered(area: Area2D) -> void:
 				GameProgress.trigger_dialog_one()
 			print("Collision detected with another falling object of the same ID! ", texture_id)
 			ScoreBoard.add_score(score_value)  # Update the global score when collision occurs
+			queue_free()
 			
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	ScoreBoard.add_score(-score_value)
--- game_progress.gd
+++ game_progress.gd
@@ -5,6 +5,7 @@ var falling_speed: float = 100
 
 # variables for the game progress
 var grabbed_for_the_first_time = false
+var counter_dialog_one = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -18,9 +19,12 @@ func increase_speed():
 	falling_speed += 5
 	
 func trigger_dialog_one():
-	GameProgress.grabbed_for_the_first_time = true
-	get_tree().paused = true
-	print("paused")
-	var main_scene = get_tree().current_scene
-	if main_scene.has_method("show_dialog_one"):
-		main_scene.show_dialog_one()
+	counter_dialog_one += 1
+	print("One: " , counter_dialog_one)
+	if counter_dialog_one == 4:
+		GameProgress.grabbed_for_the_first_time = true
+		get_tree().paused = true
+		print("paused")
+		var main_scene = get_tree().current_scene
+		if main_scene.has_method("show_dialog_one"):
+			main_scene.show_dialog_one()
