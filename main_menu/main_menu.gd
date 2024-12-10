5254a67b09f0c0bc78bc2a8fbe57d640c3ac2caa
Lukasz <Lukasz.szupka@student.fh-kiel.de>
2024-12-10 23:14:30 +0100

    forgot get_tree().paused = false

--- main_menu/main_menu.gd
+++ main_menu/main_menu.gd
@@ -5,7 +5,5 @@ func _on_start_pressed() -> void:
 	await TransitionScreen.on_transition_finished
 	get_tree().change_scene_to_file("res://main_scene/main_scene.tscn")
 
-
-
 func _on_exit_pressed() -> void:
 	get_tree().quit()
--- main_scene/pause.gd
+++ main_scene/pause.gd
@@ -6,6 +6,7 @@ func _on_continue_pressed() -> void:
 
 
 func _on_exit_pressed() -> void:
+	get_tree().paused = false
 	GameProgress.emotional_level = 70
 	GameProgress.falling_speed = 100
 	get_tree().change_scene_to_file("res://main_menu/main_menu.tscn")
